752165eaeea434740606d5f435ac450663466289
Tommy Nguyen <khenchaynguyen@gmail.com>
2024-04-15 16:38:49 +0700

    up

---
+++ MvcShop/src/main/java/com/mytech/mvcshop/config/WebMvcConfig.java
@@ -0,0 +1,40 @@
+package com.mytech.mvcshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.mytech.mvcshop")
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Bean
+	ViewResolver getViewResolver() {
+		final InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		
+		return bean;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		WebMvcConfigurer.super.addResourceHandlers(registry);
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources");
+		registry.addResourceHandler("/upload/**").addResourceLocations("/upload");
+	}
+
+	@Bean
+	StandardServletMultipartResolver getMultipartResolver() {
+		return new StandardServletMultipartResolver();
+	}
+}
---
+++ MvcShop/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,32 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+pageEncoding="UTF-8"%> <%@ taglib prefix="c" uri="jakarta.tags.core" %>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>MVC shop</title>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="container mt-5">
+      <h1>MVC shop</h1>
+
+      <h2>
+        <a href="books">Book management</a>
+      </h2>
+
+      <h3>Access server on: ${serverTime}.</h3>
+    </div>
+    
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+      integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
